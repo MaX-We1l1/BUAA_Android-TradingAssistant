2d57738c9aef3653eb9d5ccc1fae38cad0dad0ba
chenyifei <22373462@buaa.edu.cn>
2024-10-28 22:43:18 +0800

    fix重复添加

--- Market/app/src/main/java/com/example/myapplication/square/CommodityListActivity.java
+++ Market/app/src/main/java/com/example/myapplication/square/CommodityListActivity.java
@@ -34,16 +34,16 @@ public class CommodityListActivity extends AppCompatActivity {
 
         // 初始化商品列表
         //TODO 每次都会多添加两个,添加得放在其他地方
-        commodityList = new ArrayList<>();
-        DBFunction.addCommodity("商品1", "卖家1","2024-10-21",
-                Type.BOOK, 100.0F, "学生自用，几乎全新，可小刀，屠龙刀勿扰");
-        Commodity commodity1 = LitePal.find(Commodity.class, 1);
-        commodityList.add(commodity1);
-
-        DBFunction.addCommodity("商品2", "卖家2","2024-10-21",
-                Type.BAG, 1000.0F, "重生之我在bh卖包包，十年前我被仇家算害，家财散尽，如今重生归来，当我凑够十万就会前去复仇，欢迎买我的包包资助我完成我的复仇大计");
-        Commodity commodity2 = LitePal.find(Commodity.class, 2);
-        commodityList.add(commodity2);
+//        commodityList = new ArrayList<>();
+//        DBFunction.addCommodity("商品1", "卖家1","2024-10-21",
+//                Type.BOOK, 100.0F, "学生自用，几乎全新，可小刀，屠龙刀勿扰");
+//        Commodity commodity1 = LitePal.find(Commodity.class, 1);
+//        commodityList.add(commodity1);
+//
+//        DBFunction.addCommodity("商品2", "卖家2","2024-10-21",
+//                Type.BAG, 1000.0F, "重生之我在bh卖包包，十年前我被仇家算害，家财散尽，如今重生归来，当我凑够十万就会前去复仇，欢迎买我的包包资助我完成我的复仇大计");
+//        Commodity commodity2 = LitePal.find(Commodity.class, 2);
+//        commodityList.add(commodity2);
 
         // 添加更多商品...
 //        int count = LitePal.count(Commodity.class);
