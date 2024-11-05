d83ad45588bb8e79f03839fa3e61ad88b876d8d7
pc <2434491564@qq.com>
2024-11-05 21:17:39 +0800

    update settings

--- Market/app/src/main/java/com/example/myapplication/profile/PrivacyPolicyActivity.java
+++ Market/app/src/main/java/com/example/myapplication/profile/PrivacyPolicyActivity.java
@@ -16,14 +16,13 @@ public class PrivacyPolicyActivity extends AppCompatActivity {
 
         Button agree_button = findViewById(R.id.agree_button);
         agree_button.setOnClickListener(v -> {
-            Intent intent = new Intent(PrivacyPolicyActivity.this, SettingsActivity.class);
-            startActivity(intent);
             finish();
         });
 
         Button exit_button = findViewById(R.id.exit_button);
         exit_button.setOnClickListener(v -> {
             Intent intent = new Intent(PrivacyPolicyActivity.this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
             startActivity(intent);
             finish();
         });
